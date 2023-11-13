f3d134bfbcdd6cf89d69ea2355d5fc0ac1f77897
Malik Noman khan <120743420+Noman-Khan0782@users.noreply.github.com>
2023-11-13 11:31:42 +0500

    Add type conversion

--- khan.js
+++ khan.js
@@ -24,4 +24,46 @@
     }
     // console.log("V1",v2)Error we cannot define const
     console.log("V1",v1)
+    console.log("-----------------------------")
+    
     }
+
+    {
+        // #####var#####
+        var v1 = "string";
+        {
+        console.log("V1",v1) 
+        var v2="string 2";
+        console.log("V2",v2)
+        var v1="string3"; 
+        
+        }
+        console.log("V1",v1)
+        console.log("V2",v2)
+        console.log("-----------------------------")
+        
+        }
+{
+    
+        // ------------Type Conversion------
+        var a="1";
+        var b=1;
+    var c=a+b;
+    console.log(c)
+}
+{
+    
+    // ------------Type Conversion------
+    var a="1";
+    var b=1;
+var c=a-b;
+console.log(c)
+}
+{
+    
+    // ------------Type Conversion------
+    var a=1;
+    var b=1;
+var c=a+b;
+console.log(c)
+}
